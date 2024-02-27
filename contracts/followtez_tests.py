9e5c00d38991c0ea7e9599f7436d7b89450c8ef3
spangin <48512935+spangin@users.noreply.github.com>
2024-02-28 00:40:00 +0300

    Update followtez_tests.py

--- contracts/followtez_tests.py
+++ contracts/followtez_tests.py
@@ -17,12 +17,11 @@ def test():
     followtez = FollowTez()
     scenario += followtez
 
-    followtez.follow(userA.address).run(sender = userC)
+    followtez.follow(address = userA.address, is_source = False).run(sender = userC)
     scenario.verify(followtez.check(sp.record(l = userA.address, f = userC.address)))
-    followtez.follow(userA.address).run(sender = userC, valid = False)
-    followtez.follow(userA.address).run(sender = userB)
-    followtez.follow(userB.address).run(sender = userC)
-    followtez.unfollow(userB.address).run(sender = userC)
-    followtez.unfollow(userA.address).run(sender = userC)
-    followtez.unfollow(userC.address).run(sender = userA, valid = False)
-    
+    followtez.follow(address = userA.address, is_source = False).run(sender = userC, valid = False)
+    followtez.follow(address = userA.address, is_source = False).run(sender = userB)
+    followtez.follow(address = userB.address, is_source = False).run(sender = userC)
+    followtez.unfollow(address = userB.address, is_source = False).run(sender = userC)
+    followtez.unfollow(address = userA.address, is_source = False).run(sender = userC)
+    followtez.unfollow(address = userC.address, is_source = False).run(sender = userA, valid = False)
